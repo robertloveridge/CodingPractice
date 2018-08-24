802f36f41fae0661be8e942a5fc620804e81a3d2
Robert Loveridge <roberterniereg@gmail.com>
2018-08-24 22:35:19 +0100

    Simple script that fetches data from an SQLlite DB and outputs to a CSV

---
+++ perl/sql_to_csv.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use DBI;
+use File::Temp;
+use File::Copy;
+use Text::CSV_XS;
+use Getopt::Long;
+
+my $db_name = q{transactions.db};
+my $sql = q{SELECT * FROM transactions};
+my $output_path = q{/Users/rob/Documents};
+my $output_name = q{transactions.csv};
+
+GetOptions (
+  "db_name=s" => \$db_name,
+  "sql=s" => \$sql,
+  "output_path=s" => \$output_path,
+  "output_name=s" => \$output_name)
+  or die("Error in command line arguments\n");
+
+my $dsn = sprintf("dbi:SQLite:dbname=%s",$db_name);
+
+# no username or password set
+my $dbh = DBI->connect($dsn, q{}, q{}, { RaiseError => 1 })
+   or die $DBI::errstr;
+
+my $results = $dbh->prepare($sql);
+$results->execute;
+
+my $fh = File::Temp->new;
+my $csv = Text::CSV_XS->new({
+ binary 	=> 1,
+ eol 	=> "\r\n",
+});
+
+my @header_row = @{$results->{NAME}};
+$csv->print($fh, \@header_row);
+
+while (my $row = $results->fetchrow_hashref) {
+	my @row = @{$row}{@header_row};
+	$csv->print($fh, \@row);
+}
+
+$fh->flush;
+my $output = $output_path . q{/} . $output_name;
+copy($fh->filename, $output);
+
+exit();
