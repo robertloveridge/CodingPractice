23cec9754b0d63f5850fa0e6aa7015284d2656ef
Robert Loveridge <roberterniereg@gmail.com>
2025-10-26 09:35:42 +0000

    more cleanup on the Adestra examples

--- python/adestra_api/contact_create.py
+++
@@ -1,8 +0,0 @@
-import xmlrpclib
-
-server = xmlrpclib.Server("https://account.username:password@app.adestra.com/api/xmlrpc")
-
-# create a new contact on Core Table ID 1
-new_contact = server.contact.create(1, {"email":"email@domain.com"})
-
-print 'Your new or existing Contact ID', new_contact
--- python/adestra_api/workspace_get.py
+++
@@ -1,9 +0,0 @@
-import xmlrpclib
-
-server = xmlrpclib.Server("https://account.username:password@app.adestra.com/api/xmlrpc")
-
-workspaces = server.workspace.all()
-
-# display all the workspaces in the account
-for workspace in workspaces:
-        print workspace['id'], '-', workspace['name']
